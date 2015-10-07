dd72a9c54e7459b5f4d3586ebaceef9b747d8a21
reet24 <reetima.malhotra@gmail.com>
2015-10-07 16:35:18 +0530

    variables
    value of variables are getting from user and addition is performed

--- math.c
+++ math.c
@@ -2,6 +2,12 @@
 #include<conio.h>
 void main()
 {
-
+int a,b,c;
+printf("Enter value of 1st number:");
+scanf("%d",&a);
+printf("Enter value of 2nd number:");
+scanf("%d",&b);
+c=a+b;
+printf("value of c is: %d" ,c);
 getch();
 }
